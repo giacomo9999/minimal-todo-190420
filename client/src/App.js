77a4b6ecdebefa7a9b7b1b6b4b7a81a69d201f31
James Gary <gmarchesi888@gmail.com>
2019-04-21 15:00:57 -0400

    Starting to build components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import Todo from "./Todo.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          
+          <Todo />
         </header>
       </div>
     );
